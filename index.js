b49993a8a4997e4e5d9ab610d90e681b2f10d6ca
Dallas Strube <strubedallas@gmail.com>
2022-03-09 09:51:18 -0500

    part 3

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const imageWelcome = require("./imageWelcome");
+// ---------------------------------------------------------------------
+// code from Part 1 & Part 2
+
+// const imageWelcome = require("./imageWelcome");
+// ---------------------------------------------------------------------
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"],
 });
 
-client.on("ready", () => {
-  console.log(`Loged in as ${client.user.tag}`);
-});
+let bot = {
+  client,
+  prefix: "n.",
+  owners: ["328619106058108938"],
+};
 
-client.on("messageCreate", (message) => {
-  if (message.content == "hi") {
-    message.reply("Hello World!");
-  }
-});
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-const botLogsId = "946769680163536947";
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
 
-client.on("guildMemberAdd", async (member) => {
-  const img = await imageWelcome(member);
-  member.guild.channels.cache.get(botLogsId).send({
-    content: `<@${member.id}> Welcome to the server!`,
-    files: [img],
-  });
-});
+client.loadEvents(bot, false);
+
+module.exports = bot;
+
+// ---------------------------------------------------------------------
+// code from Part 1 & Part 2
+
+// client.on("ready", () => {
+//   console.log(`Loged in as ${client.user.tag}`);
+// });
+
+// client.on("messageCreate", (message) => {
+//   if (message.content == "hi") {
+//     message.reply("Hello World!");
+//   }
+// });
+
+// const botLogsId = "946769680163536947";
+
+// client.on("guildMemberAdd", async (member) => {
+//   const img = await imageWelcome(member);
+//   member.guild.channels.cache.get(botLogsId).send({
+//     content: `<@${member.id}> Welcome to the server!`,
+//     files: [img],
+//   });
+// });
+
+// ---------------------------------------------------------------------
 
 client.login(process.env.TOKEN);
